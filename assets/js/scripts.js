d6080846990724f911c14868a3b4c211693778b5
azizultex <azizultex@gmail.com>
2017-04-26 00:32:45 +0600

    reverse script.js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,4 +13,56 @@ jQuery(function($){
 	$('body').on('click', '.remove_location', function(){
 		$(this).parent().remove();
 	})
+	// START HIDE AND SHOW ELEMENTS DEPENDING ON WHETHER USER IS SCROLLING DOWN OR UP
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.job_filters').outerHeight();
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+		// Make sure they scroll more than delta
+		if(Math.abs(lastScrollTop - st) <= delta)
+			return;
+		// If they scrolled down and are past the job filters, add class .nav-up.
+		// This is necessary so you never see what is "behind" the navbar.
+		if (st > lastScrollTop && st > navbarHeight){
+			// Scroll Down
+			$('.job_filters').removeClass('nav-down').addClass('nav-up');
+		} else {
+			// Scroll Up
+			if(st + $(window).height() < $(document).height()) {
+				$('.job_filters').removeClass('nav-up').addClass('nav-down');
+			}
+		}
+		lastScrollTop = st;
+	}
+	// START HIDE AND SHOW ELEMENTS DEPENDING ON WHETHER IT'S THE TOP OF THE PAGE OR NOT
+	$(document).scroll(function() {
+	   if($(window).scrollTop() === 0) {
+		 // If it's the top of the page (only possible when it's been scrolled and then back up).
+		 $(".page-listings .map").removeClass('scrolled-page-element').addClass('top-page-element');
+		 $(".job_filters").removeClass('scrolled-page-element').addClass('top-page-element');
+	   }
+	   else {
+		 // If user has scrolled down the page.
+		 $(".page-listings .map").removeClass('top-page-element').addClass('scrolled-page-element');
+		 $(".job_filters").removeClass('top-page-element').addClass('scrolled-page-element');
+	   }
+	});
+	// IF BODY CONTAINS THESE CLASSES THEN, SHOW THE BACK TO TOP BUTTON
+	if( document.body.className.match('page-listings') ) {
+		$( ".scroll-back-to-top-wrapper" ).show();
+	}
+	if( document.body.className.match('tax-job_listing_category') ) {
+		$( ".scroll-back-to-top-wrapper" ).show();
+	}
 });
