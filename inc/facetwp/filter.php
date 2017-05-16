ca446733428249c646e0442936c85edc7b6f43fa
azizultex <azizultex@gmail.com>
2017-05-16 22:13:47 +0600

    load only listings except basic ones

--- inc/facetwp/filter.php
+++ inc/facetwp/filter.php
@@ -1,21 +1,21 @@
 <?php 
 
 // exclude basic package listings in explore page.
+function get_basic_listing_ids() {
+	$args = array(
+		'post_type' => 'job_listing',
+		'posts_per_page' => -1,
+		'meta_key'	=> '_package_id',
+		'meta_value'	=> 213,
+		'meta_compare'	=> '=',
+	);
+
+	$ALLposts = get_posts($args);
+	$ids = wp_list_pluck($ALLposts, 'ID');
+	return $ids;
+}
+
 add_filter( 'facetwp_query_args', function( $query_args, $class ) {
-        $query_args['meta_key'] = '_package_id';
-        $query_args['meta_value'] = 213; // 
-        $query_args['meta_compare'] = '=';
+        $query_args['post__not_in'] = get_basic_listing_ids();
     return $query_args;
 }, 10, 2 );
-
-$args = array(
-	'post_type' => 'job_listing',
-	'posts_per_page' => -1,
-	'meta_key'	=> '_package_id',
-	'meta_value'	=> 213,
-	'meta_compare'	=> '!=',
-);
-
-$ALLposts = get_posts($args);
-
-var_dump($ALLposts);
