424d6e5b80042f0d055b56efbd010a1efb9f42ce
azizultex <azizultex@gmail.com>
2017-05-16 22:00:07 +0600

    uploading all changes and testing

--- inc/facetwp/filter.php
+++ inc/facetwp/filter.php
@@ -1,9 +1,21 @@
 <?php 
 
-// Sort by title if the shortcode template is named "bravo"
+// exclude basic package listings in explore page.
 add_filter( 'facetwp_query_args', function( $query_args, $class ) {
         $query_args['meta_key'] = '_package_id';
-        $query_args['meta_value'] = 213;
-        $query_args['meta_compare'] = '!=';
+        $query_args['meta_value'] = 213; // 
+        $query_args['meta_compare'] = '=';
     return $query_args;
 }, 10, 2 );
+
+$args = array(
+	'post_type' => 'job_listing',
+	'posts_per_page' => -1,
+	'meta_key'	=> '_package_id',
+	'meta_value'	=> 213,
+	'meta_compare'	=> '!=',
+);
+
+$ALLposts = get_posts($args);
+
+var_dump($ALLposts);
